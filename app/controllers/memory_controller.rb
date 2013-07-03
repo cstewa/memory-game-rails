aeace7c94d021e65e77d00da3c78f7c15d325ff0
Christina <christina.v.stewart@gmail.com>
2013-07-02 18:35:47 -0700

    converted memory game into my own rails app with my own photos

---
+++ app/controllers/memory_controller.rb
@@ -0,0 +1,17 @@
+class MemoryController < ApplicationController
+  def index
+  	@image_urls = ["https://sphotos-b.xx.fbcdn.net/hphotos-frc1/575487_2394568503308_931163361_n.jpg", 
+  		"https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash3/537257_2385078106054_966445236_n.jpg", 
+  		"https://sphotos-b.xx.fbcdn.net/hphotos-ash4/389369_1950168633589_815725638_n.jpg", 
+  		"https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash3/179185_2415914916955_1540313383_n.jpg",
+  		"https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash3/922807_10200456950160635_67388911_n.jpg",
+  		"https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-prn2/225750_2007193464109_1771509319_n.jpg",
+  		"https://sphotos-b.xx.fbcdn.net/hphotos-frc1/575487_2394568503308_931163361_n.jpg", 
+  		"https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash3/537257_2385078106054_966445236_n.jpg", 
+  		"https://sphotos-b.xx.fbcdn.net/hphotos-ash4/389369_1950168633589_815725638_n.jpg", 
+  		"https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash3/179185_2415914916955_1540313383_n.jpg",
+  		"https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash3/922807_10200456950160635_67388911_n.jpg",
+  		"https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-prn2/225750_2007193464109_1771509319_n.jpg",
+  	 ]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Games::Application.config.secret_token = '88ea38a1cdbbfc953d9389b7c0c2488389eb7e75c6d668e334002b3633a8ffeddf722b65b05dfa3c8fafdbc5f91a446a2736c3875780706ff1a828a06a1f6522'
